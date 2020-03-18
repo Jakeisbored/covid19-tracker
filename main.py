e6b153ae5d39bb8ed3fd817662c99e0710a03cde
PR -Jake <43014732+Jakeisbored@users.noreply.github.com>
2020-03-18 20:56:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -83,6 +83,7 @@ async def latest_news(ctx):
           embed=discord.Embed(description="**{}**".format(chunk.replace('\xa0','\n').replace('[source]','')),  color=discord.Colour(value=16730698))
           embed.set_footer(text=cr,icon_url=client.user.avatar_url)
           await ctx.send(embed=embed)
+          """
 @client.command(brief='Get the deaths log starting from the outbreak day',description='Get the deaths log starting from the outbreak day')
 async def death_log(ctx):
   days = []
@@ -101,4 +102,5 @@ async def death_log(ctx):
   embed.set_image(url="attachment://line.png")
   embed.set_footer(text=cr,icon_url=client.user.avatar_url)
   await ctx.send(embed=embed,file=file)
+  """
 client.run('NTc2MTEzNjg5MzI1NzMxODky.Xm96Aw.TcQZx4WcGY3B_dXf8Fd4GMA3nRo')
