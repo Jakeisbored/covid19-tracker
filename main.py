2c4813a50063aed75e8df52de2846a4f036a6f0c
PR -Jake <43014732+Jakeisbored@users.noreply.github.com>
2020-03-18 14:41:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -73,11 +73,10 @@ async def covid(ctx,country:str=None):
       await ctx.send(str(e))
 @client.command(brief='Get latest info about COVID19 (Updates every 24 hours)',description='Get latest info about COVID19 (Updates every 24 hours)')
 async def latest_news(ctx):
-    if len(get_latest_info()['info']) > 2048 :
-      await ctx.send("COVID19 Latest news : {} \n **{}**".format(get_latest_info()['date'],get_latest_info()['info'].replace('\xa0','\n').replace('[source]','')))
-    else: 
-      embed=discord.Embed(title="COVID19 Latest news : {}".format(get_latest_info()['date']) , description="**{}**".format(get_latest_info()['info'].replace('\xa0','\n').replace('[source]','')),  color=discord.Colour(value=16730698))
-      embed.set_footer(text=cr,icon_url=client.user.avatar_url)
-      await ctx.send(embed=embed)
+      out = [(get_latest_info()['info'][i:i+n]) for i in range(0, len(get_latest_info()['info']), 2048)]
+      for chunk in out:
+        embed=discord.Embed(title="COVID19 Latest news : {}".format(get_latest_info()['date']) , description="**{}**".format(chunk.replace('\xa0','\n').replace('[source]','')),  color=discord.Colour(value=16730698))
+        embed.set_footer(text=cr,icon_url=client.user.avatar_url)
+        await ctx.send(embed=embed)
 
 client.run('NTc2MTEzNjg5MzI1NzMxODky.Xm96Aw.TcQZx4WcGY3B_dXf8Fd4GMA3nRo')
