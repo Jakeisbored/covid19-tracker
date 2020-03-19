e2eb573a50866ac3dcbdc491711d8e0250861691
PR -Jake <43014732+Jakeisbored@users.noreply.github.com>
2020-03-19 17:26:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -88,16 +88,28 @@ async def death_log(ctx,type:str):
   if type.replace('-text','') == 'daily':
     if type.endswith('-text'):
       days = []
+      msg = 'Daily deaths log :'
       for day in get_stats('deaths')['death_log']['daily'] :
-        days.append(int(get_stats('deaths')['death_log']['daily'][day]['total_deaths'].replace(',','')))
-      for day in days:
-        await ctx.send(day)
+        msg = '\n **{}** : **__{}__** : **{}** \n'.format(day,get_stats('deaths')['death_log']['daily'][day]['total_deaths'],get_stats('deaths')['death_log']['daily'][day]['total_change_percentage'])
+      out = [(msg[i:i+2048]) for i in range(0, len(msg), 2048)]
+      for index,chunk in enumerate(out):
+        if index == 0:
+          embed=discord.Embed(title="Daily death log",description="**{}**".format(chunk.replace('\xa0','\n').replace('[source]','')),  color=discord.Colour(value=16730698))
+          embed.set_footer(text=cr,icon_url=client.user.avatar_url)
+          await ctx.send(embed=embed)
+        else :
+          embed=discord.Embed(description="**{}**".format(chunk.replace('\xa0','\n').replace('[source]','')),  color=discord.Colour(value=16730698))
+          embed.set_footer(text=cr,icon_url=client.user.avatar_url)
+          await ctx.send(embed=embed)
     else:
       days = []
+      change = []
       for day in get_stats('deaths')['death_log']['daily'] :
         days.append(int(get_stats('deaths')['death_log']['daily'][day]['total_deaths'].replace(',','')))
+      for day in get_stats('deaths')['death_log']['daily'] :
+        change.append(int(get_stats('deaths')['death_log']['daily'][day]['total_change'].replace(',','')))
       import matplotlib.pyplot as plt
-      plt.plot(days,days, color='red')
+      plt.plot(days,change, color='red')
       plt.xlabel('Days')
       plt.ylabel('Daily deaths')
       plt.title('Daily deaths in the days following the outbreak')
