b4bf7307f81bea029d4b9684f82329ce60e17e7f
PR -Jake <43014732+Jakeisbored@users.noreply.github.com>
2020-03-19 16:47:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ async def latest_news(ctx):
 @client.command(brief='Get the deaths log starting from the outbreak day',description='Get the deaths log starting from the outbreak day')
 async def death_log(ctx,type:str):
   if type.replace('-text','') == 'daily':
-    if type.endsWith('-text'):
+    if type.endswith('-text'):
       for day in get_stats('deaths')['death_log']['daily'] :
         days.append(int(get_stats('deaths')['death_log']['daily'][day]['total_deaths'].replace(',','')))
       for day in days:
