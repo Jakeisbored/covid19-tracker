f1aa601b211f54b6e4590487b9151db0e6023fde
PR -Jake <43014732+Jakeisbored@users.noreply.github.com>
2020-03-19 21:15:20 +0100

    Update util.py

--- Cogs/util.py
+++ Cogs/util.py
@@ -1,15 +1,15 @@
 import discord
 from discord.ext import commands
+cr = "Powered by discord.py and Jake's brain"
 class Util(commands.Cog):
   def __init__(self,client):
     self.client = client
   @commands.command()
-  @commands.has_permissions(add_reactions=True,embed_links=True)
   async def help(self,ctx,*cog):
       """Gets all cogs and commands of mine."""
       try:
           if not cog:
-              halp=discord.Embed(title='Cog Listing and Uncatergorized Commands',
+              halp=discord.Embed(color=discord.Colour(value=16730698),title='Cog Listing and Uncatergorized Commands',
                                  description='Use `!help *cog*` to find out more about them!\n(BTW, the Cog Name Must Be in Title Case, Just Like this Sentence.)')
               cogs_desc = ''
               for x in self.bot.cogs:
@@ -20,28 +20,31 @@ class Util(commands.Cog):
                   if not y.cog_name and not y.hidden:
                       cmds_desc += ('{} - {}'.format(y.name,y.help)+'\n')
               halp.add_field(name='Uncatergorized Commands',value=cmds_desc[0:len(cmds_desc)-1],inline=False)
-              await ctx.message.add_reaction(emoji='✉')
-              await ctx.message.author.send('',embed=halp)
+              halp.set_footer(text=cr,icon_url=self.client.user.avatar_url)
+              await ctx.send(embed=halp)
           else:
               if len(cog) > 1:
-                  halp = discord.Embed(title='Error!',description='That is way too many cogs!',color=discord.Color.red())
-                  await ctx.message.author.send('',embed=halp)
+                  halp = discord.Embed(title='Error!',description='That is way too many cogs!',color=discord.Colour(value=16730698))
+              halp.set_footer(text=cr,icon_url=self.client.user.avatar_url)
+                  await ctx.send(embed=halp)
               else:
                   found = False
                   for x in self.bot.cogs:
                       for y in cog:
                           if x == y:
-                              halp=discord.Embed(title=cog[0]+' Command Listing',description=self.bot.cogs[cog[0]].__doc__)
+                              halp=discord.Embed(title=cog[0]+' Command Listing',description=self.bot.cogs[cog[0]].__doc__,color=discord.Colour(value=16730698))
+                              halp.set_footer(text=cr,icon_url=self.client.user.avatar_url)
                               for c in self.bot.get_cog(y).get_commands():
                                   if not c.hidden:
                                       halp.add_field(name=c.name,value=c.help,inline=False)
+                                      halp.set_footer(text=cr,icon_url=self.client.user.avatar_url)
                               found = True
                   if not found:
-                      halp = discord.Embed(title='Error!',description='How do you even use "'+cog[0]+'"?',color=discord.Color.red())
+                      halp = discord.Embed(title='Error!',description='How do you even use "'+cog[0]+'"?',color=discord.Colour(value=16730698))
+                      halp.set_footer(text=cr,icon_url=self.client.user.avatar_url)
                   else:
-                      await ctx.message.add_reaction(emoji='✉')
-                  await ctx.message.author.send('',embed=halp)
-      except:
-          pass
+                  await ctx.send(embed=halp)
+      except Exception as e:
+          await ctx.send(str(e))
 def setup(client):
   client.add_cog(Util(client))
