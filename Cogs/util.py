574a11873af84757fea71b39a5eaa32f0edd21b9
PR -Jake <43014732+Jakeisbored@users.noreply.github.com>
2020-03-19 22:20:39 +0100

    Update util.py

--- Cogs/util.py
+++ Cogs/util.py
@@ -7,10 +7,10 @@ class Util(commands.Cog):
   @commands.command()
   async def help(self,ctx,*cog):
     if not cog:
-       help=discord.Embed(title='Listing Commands', description='Use `c!help *cog*` to find out more about them! ',color=discord.Colour(value=16730698))
+       help=discord.Embed(title='Listing Commands', description='Hi , im `{}` made by `{}` . My prefixes are `c!`,`covid ` and `bot mention` . Here is a list of my commands :'.format(self.client.user.name,self.client.get_user('480407581085532180').user.name),color=discord.Colour(value=16730698))
        cmds_desc = ''
        for y in self.client.walk_commands():
-          cmds_desc += '\n **{}** : ***{}***'.format(y.name,y.description)
+          cmds_desc += '\n `{}` : ***{}***'.format(y.name,y.description if len(y.description) > 1 else 'No description found')
        help.description = help.description + cmds_desc
        help.set_footer(text=cr,icon_url=self.client.user.avatar_url)
        await ctx.send('',embed=help)
