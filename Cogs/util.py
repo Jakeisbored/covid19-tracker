02c75ec4f83e8d142138e5499f28881c009b8ff8
Jake Erin <43014732+Jakeisbored@users.noreply.github.com>
2020-03-21 19:26:18 +0100

    Update util.py

--- Cogs/util.py
+++ Cogs/util.py
@@ -7,16 +7,17 @@ class Util(commands.Cog):
   def __init__(self,client):
     self.client = client
   @commands.command()
-  async def help(self,ctx,*cog):
+  async def help(self,ctx,):
     user = self.client.get_user("480407581085532180")
     if not cog:
-       help=discord.Embed(title='Listing Commands', description='Hi , im `{}` made by `{}` . My prefixes are `c!`,`covid ` and `bot mention` . Here is a list of my commands :'.format(self.client.user.name,'Jake'),color=discord.Colour(value=16730698))
+       help=discord.Embed(title='Help menu',color=discord.Colour(value=16730698))
        cmds_desc = ''
        for y in self.client.walk_commands():
-          cmds_desc += '\n `{}` : ***{}***'.format(y.name,y.description if len(y.description) > 1 else 'No description found')
-       help.description = help.description + cmds_desc
-       help.set_footer(text=cr,icon_url=self.client.user.avatar_url)
-       await ctx.send('',embed=help)
+          cmds_desc += '\n `{}` : ***{}***'.format(y.name,y.brief if len(y.brief) > 1 else 'No brief description found')
+       help.add_field(name='Introduction:',value='Hi , im `{}` made by `{}` . My prefixes are `c!`,`covid ` and `bot mention` .'.format(self.client.user.name,'Jake'))
+       help.add_field(name='Commands and their brief descriptions:',value=cmds_desc)
+       help.set_footer(text='add a command name after the help to view more about a certain command',icon_url=self.client.user.avatar_url)
+       await ctx.send(embed=help)
 
 def setup(client):
   client.add_cog(Util(client))
