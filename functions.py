b8fad1c036d7e853a720fdf4b3b4f1c3e059a9b8
Jake Erin <43014732+Jakeisbored@users.noreply.github.com>
2020-03-25 21:31:13 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,24 +1,53 @@
 from requests import get
 from json import loads
-endpoint_v2 = 'https://coronavirus-tracker-api.herokuapp.com/v2{}'
+from bs4 import BeautifulSoup
+from math import floor
+endpoint_v1 = 'https://www.worldometers.info/coronavirus/#countries'
+html = BeautifulSoup(get(endpoint_v1).content , 'html.parser')
+# Not found class
+class NotFound(Exception):
+	"""Represents a NotFound exception"""
+	pass
+
+# Generate percentage
+def get_percentage(base_integer:int,total_integer:int):
+	return floor(base_integer * 100 / total_integer)
+
+# Worldwide infections
 def get_world_infections():
-	"""Gets infections in the world"""
-	return loads(get(endpoint_v2.format('/latest')).text)
-def get_country_list(by_country_code:bool=False):
-	"""Gets the country list mapped by id or country code"""
-	try:
-		if(by_country_code == False):
-			return [{c['country'] : c['id']} for c in loads(get(endpoint_v2.format('/locations')).text)['locations']]
-		else:
-			return [{c['country'] : c['country_code']} for c in loads(get(endpoint_v2.format('/locations')).text)['locations']]
-	except Exception:
-		raise Exception
-def get_country_infections(country_identifier:str or int):
-		for c in loads(get(endpoint_v2.format('/locations')).text)['locations']:
-			if country_identifier in c['country']:
-				return c
-			if country_identifier in c['country_code']:
-				return c
-			if str(country_identifier) in str(c['id']):
-				return c
-		raise Exception('Country is not found')
+	infections = {}
+	labels = ['country','cases','total','deaths','new_deaths','recovered','active','serious','total_per_million','deaths_per_million']
+	for tr in html.find('tbody').find_all('tr'):
+		for (index,td) in enumerate(tr.find_all('td')):
+			if (index == 0) :
+				country_key = td.getText().strip()
+				infections[country_key] = {}
+			else:
+				infections[country_key][labels[index]] = int(td.getText().replace(',','') if len(td.getText().strip()) > 0 else '0') if index in [1,2,3,5,6,7] else td.getText()
+	return infections
+
+# Country filtered infections
+def get_country_infections(country:str=None):
+	infections = {}
+	labels = ['country','cases','new_cases','deaths','new_deaths','recovered','active','serious','total_per_million','deaths_per_million']
+	for tr in html.find('tbody').find_all('tr'):
+		for (index,td) in enumerate(tr.find_all('td')):
+			if (index == 0) :
+				country_key = td.getText().strip()
+				infections[country_key] = {}
+			else:
+				infections[country_key][labels[index]] = int(td.getText().replace(',','') if len(td.getText().strip()) > 0 else '0') if index in [1,2,3,5,6,7] else td.getText() if len(td.getText().strip()) > 0 else '0'
+		if country_key == country:
+			return infections[country_key]
+	raise NotFound('No country was found')
+
+# Overall status
+def get_overall_status():
+	data = {}
+	first_labels = ['total','deaths' , 'recovered']
+	second_labels = ['mild_condition','critical_condition']
+	for (index,child) in enumerate(html.find_all('div',{'class':'maincounter-number'})):
+		data[first_labels[index]] = int(child.getText().strip().replace(',',''))
+	for (index,child) in enumerate(html.find_all('span',{'class':'number-table'})[:2]):
+		data[second_labels[index]] = int(child.getText().strip().replace(',',''))
+	return data
